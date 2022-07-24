7fe9944300267cf2512c24b8d6d8c7ac08ece7a8
Joanna Souza <joannasouza40@gmail.com>
2022-07-24 19:09:20 -0300

    Revisando,praticando e corrindo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,11 +1,14 @@
-let nome = ""
-let idade  
-console.log(typeof nome)
-console.log(typeof idade)
+let nome
+let idade;
+
+console.log(typeof nome, typeof idade)
 
 //Pq o resultado não tem valor//
 
 nome = prompt("Qual seu nome?")
 idade = prompt("Qual sua idade")
 
-console.log(typeof nome, idade)
+console.log(typeof nome, typeof Number(idade))
+
+console.log("Olá,", nome, "você tem", idade, "anos.")
+
